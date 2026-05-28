df6220d8b2399c7869074c80fb88d0294b6e071d
Alex Rivera <adanielrivera@unicesar.edu.co>
2026-05-28 10:57:57 -0500

    fix:renombrando el proyecto y paquetes

--- src/routemaster/Model/Persona.java
+++ src/routemaster/Model/Persona.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package routemap.Dao;
+package routemaster.Model;
 
 /**
  *
  * @author alexr
  */
-public class RepartidorRepository {
+public class Persona {
     
 }
